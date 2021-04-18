14439e3d9ed03ffa6649ecd2caf1b7e93bf94b14
Divya <deevyapanthi03@gmail.com>
2021-04-18 21:59:12 +0545

    initial commit

---
+++ basic-oop.dart
@@ -0,0 +1,13 @@
+class Dog{
+  String name;
+  String breed;
+  String gender;
+  int age;
+  bool isFavorite;
+  
+  
+Dog(this.name, this.breed, this.gender, this.age, this.isFavorite){
+    
+  }
+    
+}
